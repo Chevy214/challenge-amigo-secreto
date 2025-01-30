323678219f9081f2fde6d923f40998254a115128
Chevy214 <danielsebastianpb@gmail.com>
2025-01-30 16:37:34 -0600

    actualizacion primera

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresDeAmigos = []
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+ }
+
+function añadirAmigo(){
+    let nuevoNombre = (document.getElementById('amigo').value);
+   
+    if (nuevoNombre === ""){
+        alert ('La caja está vacía, intenta de nuevo')
+    } 
+    else {
+        nombresDeAmigos.push(nuevoNombre);
+        asignarTextoElemento (`ul`, `${nombresDeAmigos}`);
+         
+        console.log(nombresDeAmigos)
+    }
+}
+
+
+
