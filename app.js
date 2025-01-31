67189f161e24018bef933ef590bbe8c0f0ed5da3
Chevy214 <danielsebastianpb@gmail.com>
2025-01-30 20:13:42 -0600

    actualizado lista en pantalla

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombresDeAmigos = []
 
-function asignarTextoElemento(elemento, texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
- }
-
 function añadirAmigo(){
     let nuevoNombre = (document.getElementById('amigo').value);
-    if (nombresDeAmigos.includes(nuevoNombre)){
+        if (nombresDeAmigos.includes(nuevoNombre)){
         alert ('Ya se ingreso ese nombre, intenta de nuevo')
         limpiar ();
     }
@@ -18,8 +12,8 @@ function añadirAmigo(){
             alert ('La caja está vacía, intenta de nuevo')
         } 
         else {
-            nombresDeAmigos.push(nuevoNombre);
-            asignarTextoElemento (`ul`, `${nombresDeAmigos}`);
+            nombresDeAmigos.push(nuevoNombre);  
+            imprimirListaEnPantalla()          
             limpiar ();
             console.log(nombresDeAmigos)               
         }      
@@ -28,7 +22,7 @@ function añadirAmigo(){
 
 function sortearAmigo (){
     let numeroMaximo = nombresDeAmigos.length
-
+    
     console.log (numeroMaximo)
     let numeroAleatorio = Math.floor(Math.random()*numeroMaximo);
     console.log (numeroAleatorio)
@@ -36,7 +30,17 @@ function sortearAmigo (){
     alert (`El amigo secreto es ${nombresDeAmigos[numeroAleatorio]}`)
 }
 
+function imprimirListaEnPantalla(){
+    for (let i = 0; i < nombresDeAmigos.length; i++) {
+    const elementoPadre = document.getElementById("nombresDeAmigos");
+    let liElement = document.createElement("li");
+    elementoPadre.appendChild(liElement);
+    liElement.textContent = nombresDeAmigos[i];          
+    }
+}
+
 
 function limpiar(){
     document.getElementById('amigo').value = '';
 }
+
