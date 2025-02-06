e40eb979fa101bc4231888889e6432d7f82796ad
Chevy214 <danielsebastianpb@gmail.com>
2025-02-05 21:26:24 -0600

    Reiniciar Juego version

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let nombresDeAmigos = []
+var nombresDeAmigos = []
+let nuevoNombre = ""
+let numeroMaximo = ""
+let numeroAleatorio = ""
+let elementoHTML = ""
+
+function reiniciarJuego(){
+    for (let i = -1; i < (nombresDeAmigos.length+i); i++){
+        nombresDeAmigos.pop ();
+        }
+    
+    limpiar();
+    limpiarLista();
+    limpiarListaEnPantalla();
+    asignarTextoElemento (`resultado`, ``)
+    console.log (nombresDeAmigos)
+}
 
 function añadirAmigo(){
-    let nuevoNombre = (document.getElementById('amigo').value);
+    console.log (nombresDeAmigos)
+    nuevoNombre = (document.getElementById('amigo').value);
     nuevoNombre = nuevoNombre.toUpperCase ();
         if (nombresDeAmigos.includes(nuevoNombre)){
         alert ('Ya se ingreso ese nombre, intenta de nuevo')
@@ -26,11 +43,12 @@ function sortearAmigo (){
         if (nombresDeAmigos.length === 0 ){
         alert ("No hay ningun amigo en tu lista")
         }     else{
-        let numeroMaximo = nombresDeAmigos.length
+        numeroMaximo = nombresDeAmigos.length
         console.log (numeroMaximo)
-        let numeroAleatorio = Math.floor(Math.random()*numeroMaximo);
+        numeroAleatorio = Math.floor(Math.random()*numeroMaximo);
         console.log (numeroAleatorio)
-        asignarTextoElemento (`resultado`, `El amigo secreto es ${nombresDeAmigos[numeroAleatorio]}`)      
+        asignarTextoElemento (`resultado`, `El amigo secreto es: ${nombresDeAmigos[numeroAleatorio]}`)  
+        limpiarListaEnPantalla();        
         }
 }
 
@@ -44,8 +62,14 @@ function crearLinea(){
     }
 }
 
+function limpiarListaEnPantalla() {
+    for (let i = 0; i < nombresDeAmigos.length; i++) {
+        asignarTextoElemento (`l${i}`, '');        
+    }
+}
+
 function asignarTextoElemento(elemento, texto){
-    let elementoHTML = document.getElementById(elemento);
+    elementoHTML = document.getElementById(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
